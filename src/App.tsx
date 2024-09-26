eb068bf4f6ea500838a46883c502e53e6929e558
Pedro Miranda <113561981+pLogicador@users.noreply.github.com>
2024-09-26 17:54:55 -0300

    feat: create react app with typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pomodoro-timer.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return <div>Hello World {mainTime} </div>;
+}
+
